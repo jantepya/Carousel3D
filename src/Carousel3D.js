d7f6252e50dbef8e63dbaf898c3eb23a5716adfb
Jon Antepyan <jantepya@sfu.ca>
2019-08-23 17:37:41 -0700

    changed image icons to SVG

--- src/Carousel3D.js
+++ src/Carousel3D.js
@@ -140,9 +140,10 @@ var Carousel3D = function carousel3D () {
       var arrow_left_span = document.createElement( 'span' );
       arrow_left_helper.appendChild(arrow_left_span);
 
-      var arrow_left_img = document.createElement( 'img' );
-      arrow_left_img.src = "./src/angle-left.png";
-      arrow_left_helper.appendChild(arrow_left_img);
+      arrow_left_helper.innerHTML += '<svg height="64px" width="64px" aria-hidden="true" focusable="false" data-icon="angle-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>';
+      // var arrow_left_img = document.createElement( 'img' );
+      // arrow_left_img.src = "./src/angle-left.png";
+      // arrow_left_helper.appendChild(arrow_left_img);
 
       // --------------------
       // right arrow
@@ -153,9 +154,10 @@ var Carousel3D = function carousel3D () {
       var arrow_right_span = document.createElement( 'span' );
       arrow_right_helper.appendChild(arrow_right_span);
 
-      var arrow_right_img = document.createElement( 'img' );
-      arrow_right_img.src = "./src/angle-right.png";
-      arrow_right_helper.appendChild(arrow_right_img);
+      arrow_right_helper.innerHTML += '<svg height="64px" width="64px" aria-hidden="true" focusable="false" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>';
+      // var arrow_right_img = document.createElement( 'img' );
+      // arrow_right_img.src = "./src/angle-right.png";
+      // arrow_right_helper.appendChild(arrow_right_img);
 
 
       arrow_left_helper.addEventListener( 'click', () => { this.tileOffset -= 1; this.rotate( 500); }  , false );
